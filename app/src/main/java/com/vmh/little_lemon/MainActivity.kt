3df0a1d0bf5e03a3c57ad75a63d64acd2c9fb73a
vamsimadhav <vamsimadhav2000@gmail.com>
2023-07-08 01:19:44 +0530

    Initial Commit

---
+++ app/src/main/java/com/vmh/little_lemon/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.vmh.little_lemon
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.vmh.little_lemon.ui.theme.LittleLemonTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Text(text = "Little Lemon", fontSize = 32.sp)
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LittleLemonTheme {
+        Greeting("Android")
+    }
+}
